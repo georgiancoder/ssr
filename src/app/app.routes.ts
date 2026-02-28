b69d452d90c7d92a15be34dc627232b84c064a89
GeoCo <beqacalani@gmail.com>
2026-03-01 00:32:56 +0400

    add some pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about.component';
+import { HomePage } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePage },
+  { path: 'about', component: AboutComponent },
+];
---
+++ src/app/home/home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  template: `
+    <h1>Welcome to the Home Page</h1>
+    <p>This is your home page.</p>
+  `
+})
+export class HomePage {}
+
