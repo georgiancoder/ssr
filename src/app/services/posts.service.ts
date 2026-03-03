53112d9d9ed907acdb44a4b821874354b54458e2
GeoCo <beqacalani@gmail.com>
2026-03-03 14:26:08 +0400

    platrformID

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,15 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, PLATFORM_ID, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
   constructor(private http: HttpClient) {}
+  private platformId = inject(PLATFORM_ID);
 
   getPosts(): Observable<any[]> {
-    return this.http.get<any[]>('/.netlify/functions/posts');
+    let url = '/.netlify/functions/posts';
+    if (!isPlatformBrowser(this.platformId)) {
+      // If we are on the server, we must provide the full absolute URL.
+      // Replace this with your actual Netlify production URL.
+      url = `https://css-3d.com${url}`;
+    }
+    return this.http.get<any[]>(url);
   }
 }
 
